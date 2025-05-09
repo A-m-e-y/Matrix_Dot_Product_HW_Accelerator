6ed6a9b6bb965cd6412d53c2a977c76e3939aec0
A-m-e-y <ameyk1510@gmail.com>
2025-05-08 19:35:10 -0700

    Made eveything parameterized. Everything is working fine.
    Changes to be committed:
    	modified:   RTL/MatrixMulEngine.v
    	modified:   RTL/tb_MatrixMulEngine.v
    	modified:   matrix_result_dump.txt

--- RTL/MatrixMulEngine.v
+++ RTL/MatrixMulEngine.v
@@ -3,16 +3,23 @@
 module MatrixMulEngine #(
     parameter MAX_M = 100,
     parameter MAX_K = 100,
-    parameter MAX_N = 100
+    parameter MAX_N = 100,
+    parameter ADDR_M_BITS = $clog2(MAX_M),
+    parameter ADDR_K_BITS = $clog2(MAX_K),
+    parameter ADDR_N_BITS = $clog2(MAX_N),
+    parameter ADDR_A_BITS = $clog2(MAX_M * MAX_K),
+    parameter ADDR_B_BITS = $clog2(MAX_K * MAX_N),
+    parameter ADDR_C_BITS = $clog2(MAX_M * MAX_N),
+    parameter ADDR_WIDTH = $clog2((MAX_M > MAX_K ? (MAX_M > MAX_N ? MAX_M : MAX_N) : (MAX_K > MAX_N ? MAX_K : MAX_N)))
 )(
     input wire clk,
     input wire rst_n,
     input wire start,
     output reg done,
 
-    input [7:0] M_val,
-    input [7:0] K_val,
-    input [7:0] N_val,
+    input [ADDR_M_BITS:0] M_val,
+    input [ADDR_K_BITS:0] K_val,
+    input [ADDR_N_BITS:0] N_val,
 
     input  wire [31:0] matrix_A [0:MAX_M*MAX_K-1],
     input  wire [31:0] matrix_B [0:MAX_K*MAX_N-1],
@@ -25,21 +32,24 @@ module MatrixMulEngine #(
                STORE = 2'b10;
 
     integer i;
-    reg [7:0] row_idx, col_idx;
+    reg [ADDR_M_BITS-1:0] row_idx;
+    reg [ADDR_N_BITS-1:0] col_idx;
 
     reg  dpe_start;
     wire dpe_done;
     wire [31:0] dpe_result;
-    wire [9:0] dpe_patch_addr, dpe_filter_addr;
+    wire [ADDR_WIDTH-1:0] dpe_patch_addr, dpe_filter_addr;
     wire [31:0] dpe_patch_data, dpe_filter_data;
 
-    wire [15:0] a_index = row_idx * K_val + dpe_patch_addr;
-    wire [15:0] b_index = dpe_filter_addr * N_val + col_idx;
-    wire [15:0] c_index = row_idx * N_val + col_idx;
+    wire [ADDR_A_BITS-1:0] a_index = row_idx * K_val + dpe_patch_addr;
+    wire [ADDR_B_BITS-1:0] b_index = dpe_filter_addr * N_val + col_idx;
+    wire [ADDR_C_BITS-1:0] c_index = row_idx * N_val + col_idx;
 
-    wire [9:0] vec_len_ext = {2'b00, K_val};
+    wire [ADDR_WIDTH-1:0] vec_len_ext = K_val;
 
-    DotProductEngine dpe_inst (
+    DotProductEngine #(
+        .ADDR_WIDTH(ADDR_WIDTH)
+    ) dpe_inst (
         .clk(clk),
         .rst_n(rst_n),
         .start(dpe_start),
--- RTL/tb_MatrixMulEngine.v
+++ RTL/tb_MatrixMulEngine.v
@@ -69,12 +69,12 @@ module tb_MatrixMulEngine;
         #10;
 
         // Randomize M, K, N between 2 and 10
-        // M = $urandom_range(2, MAX_M);
-        // K = $urandom_range(2, MAX_K);
-        // N = $urandom_range(2, MAX_N);
-        M = 20;
-        K = 20;
-        N = 20;
+        M = $urandom_range(5, MAX_M);
+        K = $urandom_range(5, MAX_K);
+        N = $urandom_range(5, MAX_N);
+        // M = 50;
+        // K = 50;
+        // N = 50;
 
         // Zero out full matrices first
         for (i = 0; i < MAX_M*MAX_K; i = i + 1) matrix_A[i] = 32'h00000000;
